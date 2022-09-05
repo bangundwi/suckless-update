72cb4cb40611d9b41fdb5766031991d863e46c08
hades <hades@pop-os.localdomain>
2022-09-05 12:09:17 +0700

    suckless install

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd ~/.config/suckless/dwm/
+make
+sudo make install clean
+
+cd ~/.config/suckless/dwm/
+make
+sudo make install clean
+
+cd ~/.config/suckless/st
+make
+sudo make install clean
+
+
